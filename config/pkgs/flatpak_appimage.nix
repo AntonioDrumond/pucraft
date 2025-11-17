ccbf30ae362f0ecccb79f4d80f90fff50a9f8a7c
Antonio Drumond <adcdstom@gmail.com>
2025-11-17 20:07:36 -0300

    renato

--- config/pkgs/flatpak_appimage.nix
+++ config/pkgs/flatpak_appimage.nix
@@ -1,12 +1,5 @@
 { lib, self, ... }:
 {
-    services.flatpak = {
-        enable = false;
-        packages = [
-            # { appId = "net.waterfox.waterfox"; origin = "flathub"; }
-        ];
-    };
-
     programs.appimage = {
         enable = true;
         binfmt = true;
