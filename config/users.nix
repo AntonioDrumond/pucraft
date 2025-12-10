11ecd5aa0b844fa0cc4c63c9374c608be453d237
Antonio Drumond <adcdstom@gmail.com>
2025-12-10 11:07:02 -0300

    identify keys

--- config/users.nix
+++ config/users.nix
@@ -9,8 +9,8 @@
       ];
       shell = pkgs.fish;
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAilxUfZRgFyrYKgIyPFyL0tqIBLHNdQEzV4tzFdWYoP my-nixos-server-key"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4L3orN6izro3j6pfJy7vQ724wyS2Y+lpASapZ6VRsZ minecraft-server"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAilxUfZRgFyrYKgIyPFyL0tqIBLHNdQEzV4tzFdWYoP my-nixos-server-key" # A
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4L3orN6izro3j6pfJy7vQ724wyS2Y+lpASapZ6VRsZ minecraft-server" # M
       ];
     };
   };
